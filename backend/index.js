bdb8607d7d9a1b529dc8d6f9bc21b25b54efe756
Juan <jguardado85@gmail.com>
2021-09-04 10:01:56 -0700

    sending via node mailer

--- backend/index.js
+++ backend/index.js
@@ -7,31 +7,31 @@ const port = 3030
 // Circle back to using node mailer, we need to set up creds in
 // google and its a long process and annoying
 
-console.log(process.env.PASSWORD)
+console.log(process.env.EMAIL_USER)
 
-// let transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'sender@gmail.com',       // sender's gmail id
-//     pass: 'sender password'     // sender password
-//   }
-// });
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,       // sender's gmail id
+    pass: process.env.EMAIL_PASSWORD     // sender password
+  }
+});
 
-// var mailOptions = {
-//   from: 'sender@gmail.com',                   // sender's gmail
-//   to: 'receiver@gmail.com',                  // receiver's gmail
-//   subject: 'Sending Email using Node.js',     //subject
-//   text: 'That was easy!'                      //message Description
-// };
+var mailOptions = {
+  from: 'sender@gmail.com',                   // sender's gmail
+  to: 'titastacosinquiries@gmail.com',                  // receiver's gmail
+  subject: 'Sending Email using Node.js',     //subject
+  text: 'That was easy!'                      //message Description
+};
 
 // //send mail using transport object’s sendMail() function
-// transporter.sendMail(mailOptions, function (error, info) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// });
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
 app.get('/send', (req, res) => {
   console.log('hit send email route', req.body)
